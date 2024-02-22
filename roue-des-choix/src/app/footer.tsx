33dd2e4dca99754b238fafe0caeb634f7bf5c03f
Moquequette <105054182+PauloMoq@users.noreply.github.com>
2024-02-22 14:33:10 +0100

    img

--- roue-des-choix/src/app/footer.tsx
+++ roue-des-choix/src/app/footer.tsx
@@ -1,11 +1,13 @@
 // components/Footer.tsx
 
 import React from 'react';
+import styles from './styles/Footer.module.css';
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Cucumber Inc. Group. All rights reserved.</p>
+    <footer className={styles.footer}>
+      <p>&copy; {new Date().getFullYear()} -8px All rights reserved.</p>
+      <img src='./img/logo_trop_stylax.png' alt='coucou'/>
       {/* Add any other footer content or credits here */}
     </footer>
   );
