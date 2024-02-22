a01fcc82291264f40372c5458878d5114e343706
Moquequette <105054182+PauloMoq@users.noreply.github.com>
2024-02-22 15:07:25 +0100

    Update footer.tsx

--- roue-des-choix/src/app/footer.tsx
+++ roue-des-choix/src/app/footer.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import styles from './styles/Footer.module.css';
+import logo from  './img/logo_trop_stylax.png'
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <p>&copy; {new Date().getFullYear()} -8px All rights reserved.</p>
-      <img src='./img/logo_trop_stylax.png' alt='coucou'/>
+      <Image src={logo} alt={''} width={50} height={50}/>
       {/* Add any other footer content or credits here */}
     </footer>
   );
